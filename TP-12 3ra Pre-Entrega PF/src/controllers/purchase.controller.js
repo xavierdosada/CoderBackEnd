14ca97b8347ef30642a6f705f39f818934578e7c
Xavier Dosada <francoxavierdosada@gmail.com>
2023-08-24 13:10:53 -0300

    3era entrega FINAL 2.1

--- TP-12 3ra Pre-Entrega PF/src/controllers/purchase.controller.js
+++ TP-12 3ra Pre-Entrega PF/src/controllers/purchase.controller.js
@@ -1,10 +1,14 @@
 import { cartsRepository, productsRepository } from '../repositories/index.js'
+import { ticketMongoMgr } from '../dao/mongo/ticket.mongo.js'
+import { ticketProcess } from './ticket.controller.js'
 
 const cart_repository = cartsRepository
 const product_repository = productsRepository
+const ticket_mgr = new ticketMongoMgr()
 
 export const purchase = async (req, res) => {
     const { cid } = req.params
+    const { email } = req.user.user
     const purchase_cart = await cart_repository.getCartsById(cid)
     
     //Formateo el Proxy que devuelve mongo
@@ -18,24 +22,41 @@ export const purchase = async (req, res) => {
         const product = await product_repository.getProductById(prod.product.id);
 
         if (!product) {
-            return productsToUpdate.push({status: 'error', message: 'Producto no encontrado' });
+            productsToUpdate.push({id: product.id, status: 'No process', message: 'Producto no encontrado' });
+            continue;
         }
 
         if (prod.quantity > product.stock) {
-            return productsToUpdate.push({status: 'error', message: 'No hay suficiente stock' });
+            productsToUpdate.push({id: product.id, status: 'No process', title: product.title ,message: 'No hay suficiente stock' });
+            continue;
         }
 
         product.quantity = product.stock - prod.quantity
-        productsToUpdate.push({id: product._id, newStock: product.quantity});
+        productsToUpdate.push({id: product._id, status: 'success', newStock: product.quantity, amount: product.price});
     }
 
-    for (const prod of productsToUpdate) {
+    //Discrimino los productos procesados y los que no fueron procesados
+    const successProducts = productsToUpdate.filter(prod => prod.status === 'success')
+    const noProcessProducts = productsToUpdate.filter(prod => prod.status === 'No process')
+
+    for (const prod of successProducts) {
         try{
+            //actualizo el stock y elimino del carrito de compras los productos que fueron comprados
             await product_repository.updateProduct(prod.id, {stock: prod.newStock})
+            await cart_repository.deleteProductInCart(cid, prod.id)
         } catch(error){
             return new Error({error: error.message})
         }
     }
     
-    res.status(200).send({status: 'success', message: 'La compra fue realizada con exito!'})
+    //creación del ticket
+    const newTicket = ticketProcess(email, successProducts)
+    try {
+        const successTicket = await ticket_mgr.createTicket(newTicket)
+        res.status(200).send({status: 'success', message: 'La compra fue realizada con exito!', ticket: successTicket, noProcessProducts})
+    } catch (error) {
+        return new Error({status: 'fail', error: error.message})
+    }
+    
+    
 }
